1a9954b24a13e1c1450979b4054ac17a69367b3f
claudiaaziz <claudiahannahh@gmail.com>
2024-08-16 17:58:23 -0400

    Add gesture-based snake movement within screen bounds

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -2,12 +2,20 @@ import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 import { Colors } from '../styles/colors';
 import { PanGestureHandler } from 'react-native-gesture-handler';
 import { Coordinate, Direction, GestureEventType } from '../types/types';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Snake from './Snake';
+import { Dimensions } from 'react-native';
+import { checkGameOver } from '../utils/checkGameOver';
 
 const SNAKE_INITIAL_POS = [{ x: 5, y: 5 }];
-const FOOD_INITIAL_POS = { x: 5, y: 5 };
-const GAME_BOUNDS = { xMin: 0, xMax: 35, yMin: 0, yMax: 63 };
+const FOOD_INITIAL_POS = { x: 5, y: 20 };
+const SCREEN_DIMENSION = Dimensions.get('window');
+const GAME_BOUNDS = {
+	xMin: 0,
+	xMax: SCREEN_DIMENSION.width / 11,
+	yMin: 0,
+	yMax: SCREEN_DIMENSION.height / 11,
+};
 const MOVE_INTERVAL = 50;
 const SCORE_INCREMENT = 0;
 
@@ -15,8 +23,19 @@ export default function Game(): JSX.Element {
 	const [direction, setDirection] = useState<Direction>(Direction.Right);
 	const [snake, setSnake] = useState<Coordinate[]>(SNAKE_INITIAL_POS);
 	const [food, setFood] = useState<Coordinate>(FOOD_INITIAL_POS);
+	const [score, setScore] = useState<number>(0);
 	const [isGameOver, setIsGameOver] = useState<boolean>(false);
 	const [isPaused, setIsPaused] = useState<boolean>(false);
+
+	useEffect(() => {
+		if (!isGameOver) {
+			const intervalId = setInterval(() => {
+				!isPaused && moveSnake();
+			}, MOVE_INTERVAL);
+
+			return () => clearInterval(intervalId);
+		}
+	}, [snake, isGameOver, isPaused]);
 
 	const handleGesture = (event: GestureEventType) => {
 		const { translationX, translationY } = event.nativeEvent;
@@ -34,6 +53,35 @@ export default function Game(): JSX.Element {
 				setDirection(Direction.Up);
 			}
 		}
+	};
+
+	const moveSnake = () => {
+		const snakeHead = snake[0];
+		const newSnakeHead = { ...snakeHead };
+
+		if (checkGameOver(snakeHead, GAME_BOUNDS)) {
+			setIsGameOver((prev) => !prev);
+			return;
+		}
+
+		switch (direction) {
+			case Direction.Up:
+				newSnakeHead.y -= 1;
+				break;
+			case Direction.Down:
+				newSnakeHead.y += 1;
+				break;
+			case Direction.Left:
+				newSnakeHead.x -= 1;
+				break;
+			case Direction.Right:
+				newSnakeHead.x += 1;
+				break;
+			default:
+				break;
+		}
+
+		setSnake([newSnakeHead, ...snake.slice(0, -1)]);
 	};
 
 	return (
@@ -57,7 +105,6 @@ const styles = StyleSheet.create({
 		flex: 1,
 		borderColor: Colors.primary,
 		borderWidth: 12,
-		// borderRadius: 20,
 		borderBottomLeftRadius: 20,
 		borderBottomRightRadius: 20,
 	},
