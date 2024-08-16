32adfef5909b0a7b9c3ce2d292385fa4ce8c7a22
claudiaaziz <claudiahannahh@gmail.com>
2024-08-16 16:38:27 -0400

    Setup Game types and consts

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,17 +1,44 @@
 import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 import { Colors } from '../styles/colors';
+import { PanGestureHandler } from 'react-native-gesture-handler';
+import { GestureEventType } from '../types/types';
+
+const SNAKE_INITIAL_POS = [{ x: 5, y: 5 }];
+const FOOD_INITIAL_POS = { x: 5, y: 5 };
+const GAME_BOUNDS = { xMin: 0, xMax: 35, yMin: 0, yMax: 63 };
+const MOVE_INTERVAL = 50;
+const SCORE_INCREMENT = 0;
 
 export default function Game(): JSX.Element {
+	const handleGesture = (event: GestureEventType) => {
+		const { translationX, translationY } = event.nativeEvent;
+
+		if (Math.abs(translationX) > Math.abs(translationY)) {
+			if (translationX > 0) {
+				// moving right
+			} else {
+				// moving left
+			}
+		} else {
+			if (translationY > 0) {
+				// down
+			} else {
+				// up
+			}
+		}
+	};
+
 	return (
-		<SafeAreaView style={styles.container}>
-			<Text>Hi</Text>
-		</SafeAreaView>
+		<PanGestureHandler onGestureEvent={handleGesture}>
+			<SafeAreaView style={styles.container}>
+				<Text>Hi</Text>
+			</SafeAreaView>
+		</PanGestureHandler>
 	);
 }
 
 const styles = StyleSheet.create({
 	container: {
-		// backgroundColor: 'pink',
 		backgroundColor: Colors.primary,
 		flex: 1,
 	},
